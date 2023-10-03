74651871da0e6fdf0cbd9d2498407f86deb8ceca
Rapha2202~ <raphael.foulonbinet@gmail.com>
2023-10-03 15:02:51 +0200

    Ajout des dernieres images pour les questions ainsi que les infos.

--- index.js
+++ index.js
@@ -57,46 +57,46 @@ const questions = [
     {
         "id": 6,
         "questiontxt": "Dans quelle discipline ne trouve t-on pas de Kata?",
-        "image": "",
+        "image": "assets/kata.jpg",
         "answer1": "Karaté",
         "answer2": "Judo",
         "answer3": "Chez Maître Anthony",
         "answer4": "MMA",
         "repv": ["answer4"],
-        "info": ""
+        "info": "C'est bien en MMA qu'il n'y a pas de Kata (sacré CodeWars)"
     },
     {
         "id": 7,
         "questiontxt": "Lequel de ces acteurs n'est pas Britannique?",
-        "image": "",
-        "answer1": "Sean CONNERY",
-        "answer2": "Emma WATSON",
-        "answer3": "Cate BLANCHETT",
-        "answer4": "Idris ELBA",
+        "image": "assets/londre.jpg",
+        "answer1": "Sean Connery",
+        "answer2": "Emma Watson",
+        "answer3": "Cate Blanchett",
+        "answer4": "Idris Elba",
         "repv": ["answer3"],
-        "info": ""
+        "info": "C'est Cate Blanchett qui n'est pas Britanique"
     },
     {
         "id": 8,
         "questiontxt": "Quel est le titre de la version originale de la chanson Gangsta's paradise reprise par Coolio?",
-        "image": "",
+        "image": "assets/gangstaparadise.jpg",
         "answer1": "Present Paradise",
         "answer2": "Futur Paradise",
         "answer3": "Pastime Paradise",
         "answer4": "Ah?! Il y a une version originale?",
         "repv": ["answer3"],
-        "info": ""
+        "info": "Et oui il y a bien une version originale du nom de Pastime Paradise"
     },
     {
         "id": 9,
         "questiontxt": "Dans 'Dragon Ball', sur quelle île se déroule les championnats du monde d'arts martiaux?",
-        "image": "",
-        "answer1": "L'île papaye",
-        "answer2": "L'île kiwi",
-        "answer3": "L'île coco",
-        "answer4": "L'île bleue",
+        "image": "assets/championat.jpg",
+        "answer1": "L'île Papaya",
+        "answer2": "L'île Kiwi",
+        "answer3": "L'île Coco",
+        "answer4": "L'île Blue",
         "repv": ["answer1"],
-        "info": ""
+        "info": "C'est bien l'île Papaya et le savait tu cette île ne possède aucune autres saisons que l'été, elle possède un climat doux et chaud en permanence"
     },
     {
         "id": 10,
@@ -107,7 +107,7 @@ const questions = [
         "answer3": "Les saisons",
         "answer4": "Les doigts de la main",
         "repv": ["answer1"],
-        "info": ""
+        "info": "Il y a en bien 12 membres comme le nombres de mois dans une année"
     },
     {
         "id": 11,
@@ -118,18 +118,18 @@ const questions = [
         "answer3": "OFC 24",
         "answer4": "EA Sports FC 24",
         "repv": ["answer4"],
-        "info": ""
+        "info": "Et oui le jeux a changer de nom pour s'appeler EA Sports FC 24"
     },
     {
         "id": 12,
-        "questiontxt": "Quand est mort Michael Jackson?",
+        "questiontxt": "Quand est décédé Michael Jackson?",
         "image": "assets/michael.jpg",
         "answer1": "25 juin 2009",
         "answer2": "25 janvier 2009",
         "answer3": "25 decembre (an 1)",
         "answer4": "jamais(il vit tjr dans nos coeur)",
         "repv": ["answer1", "answer4"],
-        "info": ""
+        "info": "C'est bien le 25 Juin 2009 qu'il est décédé"
     },
     {
         "id": 13,
@@ -140,7 +140,7 @@ const questions = [
         "answer3": "Il tourne ses yeux au blancs",
         "answer4": "Il sort d'une tombe",
         "repv": ["answer3"],
-        "info": ""
+        "info": "Et oui il tourne ses yeux au blanc lors de son entrée"
     },
     {
         "id": 14,
@@ -151,73 +151,73 @@ const questions = [
         "answer3": "Point faible, trop forte",
         "answer4": "Timide jusqu'a la mort",
         "repv": ["answer1", "answer4"],
-        "info": ""
+        "info": "Elle a en effet 2 problèmes, elle s'imagine un harcelement et est extremement timide"
     },
     {
         "id": 15,
         "questiontxt": "La bonne reponse est la quatrième, choisis la",
-        "image": "assets/lequel.jpg",
+        "image": "assets/confusion.jpg",
         "answer1": "C'est une mauvaise réponse",
         "answer2": "Je ne te la conseille pas",
         "answer3": "Celle la elle sent pas bon",
         "answer4": "Celle ci c'est la meilleure",
         "repv": ["answer4"],
-        "info": ""
+        "info": "Et oui je te l'avais dit la bonne réponse est la 4eme"
     },
     {
         "id": 16,
         "questiontxt": "Parmis ces Mike, lequel a composé la musique de l'Exorciste?",
-        "image": "",
+        "image": "assets/exorciste.jpg",
         "answer1": "Mike Tyson",
         "answer2": "Mike Jagger",
         "answer3": "Mike Oldfield",
         "answer4": "Mike Maignan",
         "repv": ["answer3"],
-        "info": ""
+        "info": "C'est bien Mike Oldfield qui a composer la musique de l'exorciste"
     },
     {
         "id": 17,
         "questiontxt": "Dans l'animé Sword Art Online comment s'appel le systeme qui entretien le Jeux Vidéo dans lequel le héro est bloqué?",
-        "image": "",
+        "image": "assets/aincrad.jpg",
         "answer1": "Cardinal",
         "answer2": "Administrator",
         "answer3": "Aincrad",
         "answer4": "Yggdrasil",
         "repv": ["answer1"],
-        "info": ""
+        "info": "Le systeme porte bien le nom de Carbinal et le monde virtuel de la saison 1 est l'Aincrad que tu peux voir afficher ci contre"
     },
     {
         "id": 18,
         "questiontxt": "Quel est le meilleur language orienté objet?",
-        "image": "",
+        "image": "assets/java.jpg",
         "answer1": "PHP",
         "answer2": "JavaScript",
         "answer3": "HTML",
         "answer4": "Java",
         "repv": ["answer2"],
-        "info": ""
+        "info": "C'est bien le JavaScript le meilleur :) (Oui c'est une photo de Java et alors?)"
     },
     {
         "id": 19,
         "questiontxt": "Dans Hunter x Hunter comment est mort Meruem le roi des fourmi chimeres?",
-        "image": "",
+        "image": "assets/Meruem.jpg",
         "answer1": "Super attaque du Héro",
         "answer2": "Insecticide",
-        "answer3": "Bombe Nucleaire",
+        "answer3": "Bombe Nucléaire",
         "answer4": "Noyade",
         "repv": ["answer2"],
-        "info": ""
+        "info": "Meruem est bien mort de l'incecticide contenu dans la bombe nucléaire donc logiquement ce n'est pas la bombe qui la tuer"
     },
     {
         "id": 20,
         "questiontxt": "Dans One Piece quel est le meilleur navire?",
-        "image": "",
+        "image": "assets/one-piece.jpg",
         "answer1": "Vogue merry qui ne coule pas",
         "answer2": "Le Thousand Sunny (ca brille)",
         "answer3": "Oro Jackson (avec roger vivant)",
         "answer4": "Red Force gitanos",
         "repv": ["answer1"],
-        "info": ""
+        "info": "Et oui le meilleur bateau reste le Vogue Merry"
     }
 ]
 
